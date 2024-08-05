968dbb5c20d27111dc28d040d44017ddd0469409
Jonathan Fischoff <jonathangfischoff@gmail.com>
2024-08-05 00:36:04 +0000

    code works now

--- src/tiledMatrixMultiplication.cu
+++ src/tiledMatrixMultiplication.cu
@@ -6,10 +6,26 @@
 #include <cuda_runtime.h>
 #include "errors.h"
 
-#define TILE_WIDTH 16
-#define THREADS_PER_BLOCK_X 32
-#define THREADS_PER_BLOCK_Y 32
+#define TILE_WIDTH 4
+#define THREADS_PER_BLOCK_X 8
+#define THREADS_PER_BLOCK_Y 8
+#define min(a,b) ((a) < (b) ? (a) : (b))
 
+void slow_matrix_multiplication(float *m, float *n, float *p, int width){
+    for (int row = 0; row < width; ++row) {
+        for (int col = 0; col < width; ++col) {
+            float pValue = 0;
+            for (int k = 0; k < width; ++k) {
+                int nIndex = width*row + k;
+                int mIndex = width*k + col;
+
+                pValue += m[mIndex]*n[nIndex];
+            }
+            int pIndex = width*row + col;
+            p[pIndex] = pValue;
+        }
+    }
+}
 
 __global__ void tiledMatrixMultiplyKernel(
     float *M, 
@@ -30,20 +46,33 @@ __global__ void tiledMatrixMultiplyKernel(
     int Col = bx * TILE_WIDTH + tx;
 
     float PValue = 0;
-    if(Row < Width and Col < Width){
-        for (int ph = 0; ph < TILE_WIDTH; ++ph) {
 
-            // Colloborative loading of a tile
+    int tile_count = (Width + TILE_WIDTH - 1) / TILE_WIDTH;
+    //printf("tile_count: %d\n", tile_count);
+    
+    for (int ph = 0; ph < 1; ++ph) {
+        // Collaborative loading of a tile
+        if ((Row < Width) && (ph*TILE_WIDTH+tx) < Width){
             Mds[ty][tx] = M[Row*Width + ph*TILE_WIDTH + tx];
-            Nds[ty][tx] = N[(ph*TILE_WIDTH + ty) * Width + Col];
-            __syncthreads();
-
-            for (int k = 0; k < TILE_WIDTH; k++) {
-                PValue += Mds[ty][k] * Nds[k][tx];
-            }
-            __syncthreads();
+        } else {
+            Mds[ty][tx] = 0;
         }
+        __syncthreads();
+        if ((ph*TILE_WIDTH+ty) < Width && Col < Width) {
+            Nds[ty][tx] = N[(ph*TILE_WIDTH + ty) * Width + Col];
+        } else {
+            Nds[ty][tx] = 0;
+        }
+        __syncthreads();
 
+        for (int k = 0; k < TILE_WIDTH; ++k) {
+            PValue += Mds[ty][k] * Nds[k][tx];
+        }
+        __syncthreads();
+        
+    }
+
+    if(Row < Width && Col < Width){
         P[Row*Width + Col] = PValue;
     }
 }
@@ -82,7 +111,9 @@ void tiledMatrixMultiply(
     printf("dimBlock: (%d, %d, %d)\n", dimBlock.x, dimBlock.y, dimBlock.z);
     printf("dimGrid: (%d, %d, %d)\n", dimGrid.x, dimGrid.y, dimGrid.z);
 
+
     tiledMatrixMultiplyKernel<<< dimGrid, dimBlock >>>(N_d, M_d, P_d, width);
+
 
     cudaCheckError(cudaGetLastError());
     cudaCheckError(cudaDeviceSynchronize());
@@ -102,7 +133,7 @@ void tiledMatrixMultiply(
 }
 
 int main() {
-    int width = 4;
+    int width = 3;
     
     int m_count = width * width;
     int m_size = sizeof(float) * width;
@@ -114,17 +145,15 @@ int main() {
     int p_size = sizeof(float) * width;
 
     float M[] = {
-        1,2,3,4, 
-        5,6,7,8,
-        9,10,11,12,
-        13,14,15,16
+        1,2,3,
+        4,5,6,
+        7,8,9
     };
 
     float N[] = {
-        1,2,3,4, 
-        5,6,7,8,
-        9,10,11,12,
-        13,14,15,16
+        1,2,3,
+        4,5,6,
+        7,8,9
     };
 
     float P[p_count];
@@ -141,7 +170,23 @@ int main() {
         }
         printf("],\n");
     }
-    printf("]");
+    printf("]\n");
 
-    return 0;
+    float slowP[p_count];
+    slow_matrix_multiplication(M, N, slowP, width);
+
+    bool mismatch = false;
+    for (int i = 0; i < width; ++i) {
+        for (int j = 0; j < width; ++j) {
+            int pIndex = i*width + j;
+            float cudaValue = P[pIndex];
+            float slowValue = slowP[pIndex];
+            if (cudaValue != slowValue) {
+                printf("Index %d %d was %f for CUDA and %f for slow\n", i, j, cudaValue, slowValue);
+                mismatch = true;
+            }
+        }
+    }
+
+    return mismatch ? 1 : 0;
 }
